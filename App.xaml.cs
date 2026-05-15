4c6ebc0ba6020c2f3f6409d25b443a361e2d3e40
toneves <toneves@resume>
2026-05-15 12:09:13 -0300

    Minimal csproj + simplified App.xaml (no MergedDictionaries)

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Windows;
 
 namespace THN_Converter_Win;
@@ -8,5 +7,14 @@ public partial class App : Application
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
+
+        var resources = new ResourceDictionary
+        {
+            Source = new Uri("pack://application:,,,/Styles/DarkTheme.xaml")
+        };
+        this.Resources.MergedDictionaries.Add(resources);
+
+        var mainWindow = new MainWindow();
+        mainWindow.Show();
     }
 }
